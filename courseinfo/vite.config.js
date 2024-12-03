da994ebfe8e3b738dde10a263b7051c52c619b63
Ibukun Bakare <60797412+hibeeeekay@users.noreply.github.com>
2024-12-03 10:29:12 +0100

    Update vite.config.js

--- courseinfo/vite.config.js
+++ courseinfo/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: './courseinfo', // Set the new root directory to 'courseinfo'
+  build: {
+    outDir: '../dist', // Output the build files to a 'dist' folder outside 'courseinfo'
+  },
   plugins: [react()],
-  base: "/part_1"
-})
+});
